c8dc14add24524ce9f52e8bf7b726f1501815606
dineshroyal04 <dinesh.18mis7181@vitap.ac.in>
2023-07-14 15:35:18 +0530

    first commit

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,13 @@
+import "./ExpenseDate.css";
+export default function ExpenseDate(props) {
+  const day = props.data.toLocaleString("en-Us", { day: "2-digit" });
+  const year = props.data.getFullYear();
+  const month = props.data.toLocaleString("en-Us", { month: "long" });
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+}
