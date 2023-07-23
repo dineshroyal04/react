34441caf1821d3c940a296fdf76c34430d290d69
dineshroyal04 <dinesh.18mis7181@vitap.ac.in>
2023-07-23 06:24:19 +0530

    from 69 using states

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,17 +1,26 @@
+import React, { useState } from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Cord from "../UI/Cord";
+
+
 function ExpenseItem(props) {
-  // const { date, expense,title } = props;
-  // const month = props.date.toLocaleString('en-Us', { month: 'long' });
+
+  const [title, setTitle] = useState(props.title);
+
+  const  clickHandler = () => {
+    setTitle("updated");
+    console.log(title);
+  }
 
   return (
     <Cord className="expense-item">
       <ExpenseDate data={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <buttom onClick={clickHandler}>click me!</buttom>
     </Cord>
   );
 }
